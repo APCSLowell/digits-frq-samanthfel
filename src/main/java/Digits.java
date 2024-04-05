c6732f12e170c8e2d29f2c8acff1b10357392529
samanthfel <143027804+samanthfel@users.noreply.github.com>
2024-04-05 13:28:49 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,13 +6,24 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{ 
+	    	digitList = new ArrayList<Integer>();
+		digitList.add(0,new Integer(num%10));
+		int numRemaining = num/10;
+		while(numRemaining>0){
+			digitList.add(0,new Integer(numRemaining%10));
+			numRemaining /=10;
+		}
+	}
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	{ 
+		for(int i = 1; i < digitList.size(); i++) {
+        		if(digitList.get(i - 1).compareTo(digitList.get(i)) >= 0)
+            			return false;
+		}
+    		return true;
 
 	}
 	
